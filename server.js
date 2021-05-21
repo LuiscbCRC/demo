f3af946b5bcad844037b01f81f8216de48da5264
Luis Castillo <luiscb04@gmail.com>
2021-05-21 02:43:35 -0600

    Release

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express'); // Importing express module
 
-const app = express(); // Creating an express object
-
 const path = require('path');
 
+const app = express(); // Creating an express object
+
 const port = process.env.PORT || 8000;  // Setting an port for this application
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json())
 
 app.get('/index', function (req, res) {
     res.status(200).sendFile(path.join(__dirname+'/index.html'));
@@ -16,6 +20,13 @@ app.get('/style.css', function(req, res) {
 
 app.get('/', function (req, res) {
     res.status(200).json({"Status": "ok"});
+})
+
+app.post('/api', function (req, res){
+    res.status(200).json({
+        "Username": req.body.username,
+        "Password": req.body.password});
+    
 })
 
 // Starting server using listen function
